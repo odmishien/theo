186de168fa7e4dfbc38d7de43fbe54161d55d46d
三嶋哲也 <tmtm.yagi.007@gmail.com>
2018-04-11 22:01:05 +0900

    first commit

--- app.py
+++ app.py
@@ -10,11 +10,34 @@ from linebot.models import (
     MessageEvent,JoinEvent, TextMessage, TextSendMessage,ImageMessage,ImageSendMessage,TemplateSendMessage,ConfirmTemplate,PostbackTemplateAction,MessageTemplateAction
 )
 import datetime
+import sched
+import time
 
 app = Flask(__name__)
 
 line_bot_api = LineBotApi('6w+yDVbtosggFA+eHjGvxbdxvtiNnbo2Szpet/7pvsF2VIoNpMR29zVUGCKnheQdBWJBWk1hnNVc2UIjooUdn/vbDm6pHU2EZkG9gUXdjPkoeVUIePuKqipmQYExGPlKeQxYIVv1oU6wbtQXjMBR5gdB04t89/1O/w1cDnyilFU=')
 handler = WebhookHandler('9ae6932fad2fa65e7020d34b3d41d2a2')
+
+def shiyoya(event):
+    line_bot_api.reply_message(
+        event.reply_token,
+        TemplateSendMessage(
+            alt_text='Confirm template',
+            template=ConfirmTemplate(
+            text='明日、飲み会しよや！！',
+            actions=[
+            PostbackTemplateAction(
+                label='アリ',
+                text='アリ',
+                data='action=buy&itemid=1'
+            ),
+            MessageTemplateAction(
+                label='ナシ',
+                text='ナシ'
+            )
+        ]
+    )
+))
 
 @app.route("/callback", methods=['POST'])
 def callback():
@@ -35,27 +58,15 @@ def callback():
 def handle_message(event):
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text="画像を送ってクレメンス"))
+        TextSendMessage(text="月に2回、突然飲み会セッティングするからよろしく頼むわ！！"))
 @handler.add(JoinEvent)
 def handle_message(event):
     line_bot_api.reply_message(
         event.reply_token,
-        TemplateSendMessage(
-            alt_text='Confirm template',
-            template=ConfirmTemplate(
-            text='明日、飲み会しよや！！',
-            actions=[
-            PostbackTemplateAction(
-                label='アリ',
-                text='アリ',
-                data='action=buy&itemid=1'
-            ),
-            MessageTemplateAction(
-                label='ナシ',
-                text='ナシ'
-            )
-        ]
-    )
-))
+        TextSendMessage(text="月に2回、突然飲み会セッティングするからよろしく頼むわ！！"))
+    scheduler = sched.scheduler(time.time, time.sleep)
+    today = datetime.date.today()
+    scheduler.enter(60,1,shiyoya(event))
+    
 if __name__ == '__main__':
     app.run()
