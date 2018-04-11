74148d5faadfd871baa8a2c97747ddddfe9d6b4d
三嶋哲也 <tmtm.yagi.007@gmail.com>
2018-04-11 20:39:21 +0900

    first commit

---
+++ app.py
@@ -0,0 +1,43 @@
+from flask import Flask, request, abort
+
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage,ImageMessage,ImageSendMessage,TemplateSendMessage
+)
+import datetime
+
+app = Flask(__name__)
+
+line_bot_api = LineBotApi('6w+yDVbtosggFA+eHjGvxbdxvtiNnbo2Szpet/7pvsF2VIoNpMR29zVUGCKnheQdBWJBWk1hnNVc2UIjooUdn/vbDm6pHU2EZkG9gUXdjPkoeVUIePuKqipmQYExGPlKeQxYIVv1oU6wbtQXjMBR5gdB04t89/1O/w1cDnyilFU=')
+handler = WebhookHandler('9ae6932fad2fa65e7020d34b3d41d2a2')
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+
+    # get request body as text
+    body = request.get_data(as_text=True)
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+
+    return ''
+@handler.default()
+def default(event):
+    if event.type == "group":
+        line_bot_api.push_message(event.groupId, TextSendMessage(text='Hello World!'))
+    else if room.type == "room":
+        line_bot_api.push_message(event.groupId, TextSendMessage(text='Hello World!'))
+    else:
+        line_bot_api.push_message(event.groupId, TextSendMessage(text='グループに入れてね'))
+
+if __name__ == "__main__":
+    app.run()
