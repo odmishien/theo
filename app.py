972ea4ba4a7108d8ed5ba2565b91920d2f41e405
三嶋哲也 <tmtm.yagi.007@gmail.com>
2018-04-12 17:51:38 +0900

    first commit

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ app = Flask(__name__)
 line_bot_api = LineBotApi('6w+yDVbtosggFA+eHjGvxbdxvtiNnbo2Szpet/7pvsF2VIoNpMR29zVUGCKnheQdBWJBWk1hnNVc2UIjooUdn/vbDm6pHU2EZkG9gUXdjPkoeVUIePuKqipmQYExGPlKeQxYIVv1oU6wbtQXjMBR5gdB04t89/1O/w1cDnyilFU=')
 handler = WebhookHandler('9ae6932fad2fa65e7020d34b3d41d2a2')
 groups = []
-
+print(groups)
 @app.route("/callback", methods=['POST'])
 def callback():
     # get X-Line-Signature header value
