f95d7b3358f320d621d24b82d232320ac72f9423
三嶋哲也 <tmtm.yagi.007@gmail.com>
2018-04-12 17:33:47 +0900

    first commit

--- app.py
+++ app.py
@@ -36,9 +36,8 @@ def shiyoya(event):
     Id = event.source.user_id
     global groups
     groups.append(Id)
-    print(groups)
 
-
+print(groups)
 if __name__ == '__main__':
     app.run()
     print(groups)
