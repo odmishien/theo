aa9bd6339370f23f63e64e234a76d24ef189df45
三嶋哲也 <tmtm.yagi.007@gmail.com>
2018-04-14 17:10:42 +0900

    first commit

--- app.py
+++ app.py
@@ -9,9 +9,8 @@ from linebot.exceptions import (
 from linebot.models import (
     MessageEvent,JoinEvent, TextMessage, TextSendMessage,ImageMessage,ImageSendMessage,TemplateSendMessage,ConfirmTemplate,PostbackTemplateAction,MessageTemplateAction
 )
-import tempfile
 
-
+temp = tempfile.TemporaryFile()
 app = Flask(__name__)
 line_bot_api = LineBotApi('6w+yDVbtosggFA+eHjGvxbdxvtiNnbo2Szpet/7pvsF2VIoNpMR29zVUGCKnheQdBWJBWk1hnNVc2UIjooUdn/vbDm6pHU2EZkG9gUXdjPkoeVUIePuKqipmQYExGPlKeQxYIVv1oU6wbtQXjMBR5gdB04t89/1O/w1cDnyilFU=')
 handler = WebhookHandler('9ae6932fad2fa65e7020d34b3d41d2a2')
@@ -35,9 +34,8 @@ def callback():
 def shiyoya(event):
     Id = event.source.user_id
     print(Id)
-    f = tempfile.TemporaryFile(mode='w+t')
-    f.write(Id + "\n")
-    f.seek(0)
+    f = open("/tmp/ids.txt",'a')
+    f.write(str(Id) + "\n")
     f.close()
 
 if __name__ == '__main__':
--- shiyoya.py
+++ shiyoya.py
@@ -7,7 +7,6 @@ from linebot.exceptions import (
 from linebot.models import (
     MessageEvent,JoinEvent, TextMessage, TextSendMessage,ImageMessage,ImageSendMessage,TemplateSendMessage,ConfirmTemplate,PostbackTemplateAction,MessageTemplateAction
 )
-from app import temp
 
 line_bot_api = LineBotApi('6w+yDVbtosggFA+eHjGvxbdxvtiNnbo2Szpet/7pvsF2VIoNpMR29zVUGCKnheQdBWJBWk1hnNVc2UIjooUdn/vbDm6pHU2EZkG9gUXdjPkoeVUIePuKqipmQYExGPlKeQxYIVv1oU6wbtQXjMBR5gdB04t89/1O/w1cDnyilFU=')
 # signature = request.headers['X-Line-Signature']
@@ -19,8 +18,8 @@ line_bot_api = LineBotApi('6w+yDVbtosggFA+eHjGvxbdxvtiNnbo2Szpet/7pvsF2VIoNpMR29
 #     handler.handle(body, signature)
 # except InvalidSignatureError:
 #     abort(400)
-print(temp.read())
-ids = temp.read()
+f = open("/tmp/ids.txt","r")
+ids = f.readlines()
 print(ids)
 for id in ids:
     # num = random.randrange(30)
