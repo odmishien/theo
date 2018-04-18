65b66d0632aff5fd1797332564ddbcc2198cfe05
三嶋哲也 <tmtm.yagi.007@gmail.com>
2018-04-18 11:07:49 +0900

    db set

--- app.py
+++ app.py
@@ -16,8 +16,7 @@ line_bot_api = LineBotApi('6w+yDVbtosggFA+eHjGvxbdxvtiNnbo2Szpet/7pvsF2VIoNpMR29
 handler = WebhookHandler('9ae6932fad2fa65e7020d34b3d41d2a2')
 
 #DB接続部分
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['postgres://iiawcisrcummxi:cc6d1f943f164d81c6021a46ca6a75840685b1011854de2a16486bef3eaa9bab@ec2-54-227-251-233.compute-1.amazonaws.com:5432/dfv716iqeevbhu
-']
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['postgres://iiawcisrcummxi:cc6d1f943f164d81c6021a46ca6a75840685b1011854de2a16486bef3eaa9bab@ec2-54-227-251-233.compute-1.amazonaws.com:5432/dfv716iqeevbhu']
 db = SQLAlchemy(app)
 
 class Ids(db.Model):
