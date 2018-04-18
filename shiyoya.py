e52f4f35f624a5ac630dbb33b9ac37f585903c21
三嶋哲也 <tmtm.yagi.007@gmail.com>
2018-04-18 12:24:35 +0900

    db set

--- shiyoya.py
+++ shiyoya.py
@@ -7,6 +7,8 @@ from linebot.exceptions import (
 from linebot.models import (
     MessageEvent,JoinEvent, TextMessage, TextSendMessage,ImageMessage,ImageSendMessage,TemplateSendMessage,ConfirmTemplate,PostbackTemplateAction,MessageTemplateAction
 )
+from flask_sqlalchemy import SQLAlchemy
+from app import Ids
 
 line_bot_api = LineBotApi('6w+yDVbtosggFA+eHjGvxbdxvtiNnbo2Szpet/7pvsF2VIoNpMR29zVUGCKnheQdBWJBWk1hnNVc2UIjooUdn/vbDm6pHU2EZkG9gUXdjPkoeVUIePuKqipmQYExGPlKeQxYIVv1oU6wbtQXjMBR5gdB04t89/1O/w1cDnyilFU=')
 # signature = request.headers['X-Line-Signature']
@@ -18,25 +20,24 @@ line_bot_api = LineBotApi('6w+yDVbtosggFA+eHjGvxbdxvtiNnbo2Szpet/7pvsF2VIoNpMR29
 #     handler.handle(body, signature)
 # except InvalidSignatureError:
 #     abort(400)
-f = open("/tmp/ids.txt","r")
-ids = f.readlines()
+ids = Ids.query.all()
 print(ids)
-for id in ids:
-    # num = random.randrange(30)
-    # if num < 2:
-    line_bot_api.push_message(id,TemplateSendMessage(
-        alt_text='Confirm template',
-        template=ConfirmTemplate(
-        text='明日、飲み会しよや！！',
-            actions=[
-                PostbackTemplateAction(
-                    label='アリ',
-                    text='アリ',
-                    data='action=buy&itemid=1'
-                ),
-                MessageTemplateAction(
-                    label='ナシ',
-                    text='ナシ'
-                )
-            ]
-        )))
+# for id in ids:
+#     # num = random.randrange(30)
+#     # if num < 2:
+#     line_bot_api.push_message(id,TemplateSendMessage(
+#         alt_text='Confirm template',
+#         template=ConfirmTemplate(
+#         text='明日、飲み会しよや！！',
+#             actions=[
+#                 PostbackTemplateAction(
+#                     label='アリ',
+#                     text='アリ',
+#                     data='action=buy&itemid=1'
+#                 ),
+#                 MessageTemplateAction(
+#                     label='ナシ',
+#                     text='ナシ'
+#                 )
+#             ]
+#         )))
